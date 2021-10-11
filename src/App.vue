545f5e8bda73e51871cf8273b8143342679c20a2
mouaadev <mouaadev@gmail.com>
2021-10-11 09:31:57 +0100

    Add "Source Code" component

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <h1 class="header">
+    <i class="fas fa-border-style"></i>
+    Border Radius Previewer
+  </h1>
+  <Shape />
+</template>
+
+<script>
+import Shape from "./components/Shape.vue"
+
+export default {
+  components: {
+    Shape,
+  },
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&family=Source+Code+Pro:wght@700&display=swap");
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: "Poppins", sans-serif;
+  background-color: #232323;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+#app {
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.header {
+  font-size: 2.5rem;
+  background: linear-gradient(270deg, #d6b33a 0%, #d14141 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  margin: 1.5rem 0 1.5rem 0;
+}
+</style>
---
+++ src/components/Shape.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="rect"></div>
+  <div class="inputs">
+    <input type="text" />
+    <input type="text" />
+    <input type="text" />
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.rect {
+  width: 400px;
+  height: 400px;
+  background-color: #dfc155;
+  margin-bottom: 2rem;
+}
+
+.inputs {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.inputs input {
+  width: 80px;
+  margin-left: 1rem;
+  text-align: center;
+  font-family: inherit;
+  padding: 0.2rem;
+}
+
+.inputs input:first-child {
+  margin: 0;
+}
+</style>
