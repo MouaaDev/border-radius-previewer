f3f5a5376d5fddd40871883d59fb05db5a3fc0f7
mouaadev <mouaadev@gmail.com>
2021-10-12 08:52:12 +0100

    Add Footer component.

---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer>
+    <p>App by MouaaDev @</p>
+    <a
+      class="github"
+      href="https://github.com/MouaaDev/border-radius-previewer"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <i class="fab fa-github"></i>
+    </a>
+  </footer>
+</template>
+
+<style>
+footer {
+  position: absolute;
+  bottom: 0;
+  /* margin-top: 2.2rem; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #4d4d4d;
+  width: 100%;
+}
+
+footer p {
+  display: inline-block;
+  font-size: 0.9rem;
+  font-weight: normal;
+  color: #fff;
+}
+
+.github .fa-github {
+  font-size: 1.5rem;
+  color: #fff;
+  margin-left: 0.5rem;
+}
+</style>
